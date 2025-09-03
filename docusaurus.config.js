8bf767be2194cdbe1852e6958db9b58fb43b0c00
sammex45 <samuelemediong45@gmail.com>
2025-09-03 14:28:06 +0100

    final commit-01

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://sammex45.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Refract/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
